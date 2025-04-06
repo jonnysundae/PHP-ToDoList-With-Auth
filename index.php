656f6695dd878baed96403e0c809b51405c05d93
jonnysundae <contatojonathannascimento@gmail.com>
2025-04-06 07:00:35 -0300

    Mudando o arquivo que o formulário direciona para o novo feito em POO

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <?php
-    session_start();
-    unset($_SESSION["usuario"]);
+session_start();
+unset($_SESSION["usuario"]);
 
-    if(!empty($_SESSION["errosAoFazerLogin"])){
-        echo "<ul>";
-        foreach ($_SESSION["errosAoFazerLogin"] as $erroAoFazerLogin) {
-            echo "<li>".$erroAoFazerLogin."</li>";
-        }
-        echo "</ul>";
+if (!empty($_SESSION["errosAoFazerLogin"])) {
+    echo "<ul>";
+    foreach ($_SESSION["errosAoFazerLogin"] as $erroAoFazerLogin) {
+        echo "<li>".$erroAoFazerLogin."</li>";
     }
+    echo "</ul>";
+}
 
-    unset($_SESSION["errosAoFazerLogin"]);
+unset($_SESSION["errosAoFazerLogin"]);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
 </head>
+
 <body>
     <h1>Faça login</h1>
-    <form action="app/fazendoLogin.php" method="post">
+    <form action="app/controller/Usuario/UsuarioLoginController.php" method="post">
         <fieldset>
             <label for="email">E-mail:</label>
             <input type="email" name="email" id="email">
@@ -37,4 +39,5 @@
         <a href="novaconta/criarconta.php">Criar uma conta</a>
     </p>
 </body>
+
 </html>
