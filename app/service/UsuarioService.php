29bda628b63a2106012e2256774a3d64db0645f2
jonnysundae <contatojonathannascimento@gmail.com>
2025-04-06 07:01:34 -0300

    Criando service de Usuario

---
+++ app/service/UsuarioService.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once __DIR__ . '/../config/BancoDeDados.php';
+require_once __DIR__ . '/../model/Usuario.php';
+require_once __DIR__ . '/../exceptions/EmailNaoEncontradoException.php';
+require_once __DIR__ . '/../exceptions/SenhaIncorretaException.php';
+
+class UsuarioService
+{
+    private $bancoDeDados;
+
+    public function __construct()
+    {
+        $this->bancoDeDados = BancoDeDados::conectar();
+    }
+
+    public function buscarPorEmail($email)
+    {
+        $sql = "SELECT * FROM usuarios WHERE email = :email LIMIT 1";
+        $stmt = $this->bancoDeDados->prepare($sql);
+        $stmt->execute([':email' => $email]);
+
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+        if ($dados) {
+            return new Usuario($dados['nome'], $dados['email'], $dados['id'], $dados['senha']);
+        } else {
+            throw new EmailNaoEncontradoException();
+        }
+        
+    }
+
+    public function autenticar($email, $senha)
+    {
+        try {
+            $usuario = $this->buscarPorEmail($email);
+            if (password_verify($senha, $usuario->getSenha())) {
+                return $usuario;
+            } else {
+                throw new SenhaIncorretaException();
+            }
+        } catch (EmailNaoEncontradoException | SenhaIncorretaException $e) {
+            throw $e;
+        }
+
+    }
+
+    public function criarUsuario(Usuario $usuario)
+    {
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
+        $stmt = $this ->bancoDeDados->prepare($sql);
+        $stmt->execute([
+            ':nome' => $usuario->getNome(),
+            ':email' => $usuario->getEmail(),
+            ':senha' => password_hash($usuario->getSenha(), PASSWORD_DEFAULT)
+        ]);
+        
+    }
+
+}
